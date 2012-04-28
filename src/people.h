4e3966ad65a8a0c82f830a72f54f96504f61a31d
pond <pond@localhost>
2012-04-29 00:00:00 +0700

    Modify people_detecion

--- src/people.h
+++ src/people.h
@@ -24,3 +24,30 @@ float dist[1280][1024];
 int canPrintDepth = 0; // บางทีค่า depth มันมาช้ากว่า RGB พอเฟรมแรกแมร่งก็พัง ><
 
 IplImage* img = cvCreateImage( cvSize(1280,1024),IPL_DEPTH_8U, 1 );
+
+
+//// Function prototypes
+void learn();
+void recognize();
+void doPCA();
+void storeTrainingData();
+int  loadTrainingData(CvMat ** pTrainPersonNumMat);
+int  findNearestNeighbor(float * projectedTestFace);
+int  loadFaceImgArray(char * filename);
+void printUsage();
+IplImage* cropImage(const IplImage *img, const CvRect region);
+IplImage* resizeImage(const IplImage *origImg, int newWidth, int newHeight);
+IplImage* convertFloatImageToUcharImage(const IplImage *srcImg);
+void saveFloatImage(const char *filename, const IplImage *srcImg);
+
+CvMemStorage *storage = cvCreateMemStorage( 0 );
+CvHaarClassifierCascade *cascade  = ( CvHaarClassifierCascade* )cvLoad( "/home/skubu-athome/skuba_athome_main/people/haarcascade_frontalface_alt.xml" ,0 , 0, 0 );
+
+IplImage ** faceImgArr        = 0; // array of face images
+CvMat    *  personNumTruthMat = 0; // array of person numbers
+int nTrainFaces               = 0; // the number of training images
+int nEigens                   = 0; // the number of eigenvalues
+IplImage * pAvgTrainImg       = 0; // the average image
+IplImage ** eigenVectArr      = 0; // eigenvectors
+CvMat * eigenValMat           = 0; // eigenvalues
+CvMat * projectedTrainFaceMat = 0; // projected training faces
